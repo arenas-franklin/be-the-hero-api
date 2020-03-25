f24d8c69dd79dcf1b4e85a873065d0e75732cf4c
arenas-franklin <franklin.silva8@fatec.sp.gov.br>
2020-03-25 19:27:28 -0300

    criada rota e controle para incident

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController')
 
+const IncidentController = require('./controllers/IncidentController')
+
 const routes = express.Router();
 
 // get /ongs lista todas as ongs
@@ -10,4 +12,8 @@ routes.get('/ongs', OngController.index);
 // post /ongs realiza cadastro das ongs 
 routes.post('/ongs', OngController.create);
 
+routes.get('/incidents', IncidentController.index)
+
+//post / incidents rota para cadastrar casos (incidentes)
+routes.post('/incidents', IncidentController.create);
 module.exports = routes;
