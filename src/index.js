8bff0dd7e49986c2fdbc1f5bf435672b328b265a
arenas-franklin <franklin.silva8@fatec.sp.gov.br>
2020-03-25 20:30:40 -0300

    inserido um join com a tabela ongs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express();
 
-//informa que usará informações em json
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 
