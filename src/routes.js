a82f7f53114a6586e1c5e0ff3b5489450d20e1a7
arenas-franklin <franklin.silva8@fatec.sp.gov.br>
2020-03-25 18:45:47 -0300

    criado metodo post /ongs para cadastrar ongs

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post ('/users', (req, res)=>{
-    const body = req.body;
+// post /ongs realiza cadastro das ongs 
+routes.post('/ongs', async (request, response)=>{
+    const {name, email, whatsapp, city, uf } = request.body;
 
-    console.log(body);
+    const id = crypto.randomBytes(4).toString('HEX');
 
-    return Response.json({
-        evento:"Semana OmniStack 11.0",
-        aluno: 'Celiton Souza'
-    });
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+
+    return response.json({ id });
 });
 
 module.exports = routes;
