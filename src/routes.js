7498a344f3df010c7fd91f32f293a9eaf1fad03d
arenas-franklin <franklin.silva8@fatec.sp.gov.br>
2020-03-25 19:39:22 -0300

    criada rota e controle para deletar incident

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,12 @@ const IncidentController = require('./controllers/IncidentController')
 
 const routes = express.Router();
 
-// get /ongs lista todas as ongs
-routes.get('/ongs', OngController.index);
 
-// post /ongs realiza cadastro das ongs 
+routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
 routes.get('/incidents', IncidentController.index)
-
-//post / incidents rota para cadastrar casos (incidentes)
 routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
 module.exports = routes;
